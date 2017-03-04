4bcc5ebcd79d38ada61b260b686f64d345beb1de
RoganRhysShane <chrisoliver08@live.com>
2017-03-04 12:15:46 -0500

    method

--- src/main/java/LightSwitch.java
+++ src/main/java/LightSwitch.java
@@ -12,10 +12,19 @@
  */
 public class LightSwitch {
 
+
     // todo: add a property named "on" that indicates if the light switch is on or off. Default this property to false.
+    boolean on = false;
 
     // todo: create a public method named toggle() that toggles the switch on and off. IE: if it's on, turn it off. If it's off, turn it on.
+    public void toggle() {
+        if (on) {
+            on = false;
 
-        // todo: the body of the toggle() method should set the on property to the opposite of its current value.
+        } else {
+            on = true;
+        }
+    }
+    // todo: the body of the toggle() method should set the on property to the opposite of its current value.
 
 }
